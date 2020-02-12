bc384277e6214230aeb0faea1a888145b059267b
behnam <behnamfattahzadeh90@gmail.com>
2020-02-12 18:36:35 +0330

    sum bracket homwork

---
+++ calculatcroshe.py
@@ -0,0 +1,6 @@
+# mylist_1=[[1,7],[3,6]]
+# mylist_2=[[8,9],[0,3]]
+def mohasebat(mylist_1=[],mylist_2=[]):
+    return [mylist_1[0][0]+mylist_2[0][0],mylist_1[0][1]+mylist_2[0][1]],[mylist_1[1][0]+mylist_2[1][0],mylist_1[1][1]+mylist_2[1][1]]
+
+
---
+++ homework sum.py
@@ -0,0 +1,6 @@
+# mylist_1=[[1,7],[3,6]]
+# mylist_2=[[8,9],[0,3]]
+# print(f"sum is : [[{mylist_1[0][0]+mylist_2[0][0]},{mylist_1[0][1]+mylist_2[0][1]}],[{mylist_1[1][0]+mylist_2[1][0]},{mylist_1[1][1]+mylist_2[1][1]}]]")
+
+import calculatcroshe
+print(calculatcroshe.mohasebat(mylist_1=[[1,7],[3,6]],mylist_2=[[8,9],[0,3]]))
